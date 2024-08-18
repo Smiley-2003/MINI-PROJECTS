14c4c516aa276d17a857c3b85716b55eb7fa69bf
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-08-18 15:41:26 +0530

    Add files via upload

---
+++ JS_function_calculator/Function_calculator.js
@@ -0,0 +1,19 @@
+function add(num1,num2){
+    return num1 + num2;
+}
+function subtract(num1,num2){
+    return num1 - num2;
+}
+function multiply(num1,num2){
+    return num1 * num2;
+}
+function divide(num1,num2){
+    return num1/num2;
+}
+function calculator(num1,num2,operator){
+    return operator(num1,num2);
+}
+console.log(calculator(8,4,add));
+console.log(calculator(8,4,subtract));
+console.log(calculator(8,4,multiply));
+console.log(calculator(8,4,divide));
